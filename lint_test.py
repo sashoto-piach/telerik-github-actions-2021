e63302c1d4e31d1036e4a0caa413a48d251a15ef
aalexandrov <aalexandrov@hedgeserv.com>
2022-10-19 21:00:23 +0300

    wip-pylint

--- lint_test.py
+++ lint_test.py
@@ -4,6 +4,6 @@ THRESHOLD = 5
 run = lint.Run(["factorial.py"], do_exit=False)
 score = run.linter.stats.global_note
 if score < THRESHOLD:
-    print("Linter failed: Score < threshold value")
+    print("Linter failed: Score < threshold value.")
     sys.exit(1)
 sys.exit(0)
